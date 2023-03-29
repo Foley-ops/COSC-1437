0cde3552a7785436e457418e502facd2543276d9
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-03-28 21:20:00 -0500

    Finished project

--- CodeZinger Codes/Chapter 8/8.2.1/Static Polymorphism/src/ProblemSolution.java
+++ CodeZinger Codes/Chapter 8/8.2.1/Static Polymorphism/src/ProblemSolution.java
@@ -1,4 +1,9 @@
 public class ProblemSolution {
-    //write your code here
+    public long solution(long N1, long N2) {
+        return N1 + N2;
+    }
+    public long solution(long N1, long N2, long N3) {
+        return N1 + N2 + N3;
+    }
 
 }
