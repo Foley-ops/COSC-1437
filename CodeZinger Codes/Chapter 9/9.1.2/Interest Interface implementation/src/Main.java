8ccefe5623aa81ba605d6f78b0447b7272b8ca01
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-06 11:36:47 -0500

    Added code given by module

--- CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Main.java
+++ CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner s = new Scanner(System.in);
+        int principle = s.nextInt();
+        float year = s.nextFloat();
+        Interest bank = new Bank(principle, year);
+        System.out.println(String.format("%.2f", bank.simpleInterest()));
+        System.out.println(String.format("%.2f", bank.compInterest()));
     }
 }
