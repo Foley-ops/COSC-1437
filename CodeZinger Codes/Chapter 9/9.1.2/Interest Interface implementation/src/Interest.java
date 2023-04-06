98b672b1f990e1a9c122a0c831808fab0e4dc7ec
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-06 12:08:48 -0500

    First output prints

--- CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Interest.java
+++ CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Interest.java
@@ -1,9 +1,5 @@
-public interface Interest {
+interface Interest {
     static final int rate = 25;
-    public default double simpleInterest() {
-        return this.rate;
-    }
-    public default double compInterest() {
-        return this.rate;
-    }
+    double simpleInterest();
+    double compInterest();
 }
--- CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Main.java
+++ CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        int principle = s.nextInt();
+        int principal = s.nextInt();
         float year = s.nextFloat();
-        Interest bank = new Bank(principle, year);
-        System.out.println(String.format("%.2f", bank.simpleInterest()));
-        System.out.println(String.format("%.2f", bank.compInterest()));
+        Bank bank = new Bank(principal, year);
+
+        double simpleInterest = bank.simpleInterest();
+        double compInterest = bank.compInterest();
+
+
+        System.out.println(String.format("%.2f", simpleInterest));
+        System.out.println(String.format("%.2f", compInterest));
     }
 }
