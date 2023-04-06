9234ec83eb3e5ffbec0565391b5dfd568a5316b7
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-06 11:32:03 -0500

    Interest Interface stood up

--- CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Interest.java
+++ CodeZinger Codes/Chapter 9/9.1.2/Interest Interface implementation/src/Interest.java
@@ -1,2 +1,9 @@
 public interface Interest {
+    static final int rate = 25;
+    public default double simpleInterest() {
+        return this.rate;
+    }
+    public default double compInterest() {
+        return this.rate;
+    }
 }
