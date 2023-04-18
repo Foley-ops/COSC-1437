f64f39d50d7c0f07c5a6b75e5ad124916a8640de
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-18 12:31:43 -0500

    Finished program

--- CodeZinger Codes/Chapter 11/11.1.3/Sum of elements of an array/src/Main.java
+++ CodeZinger Codes/Chapter 11/11.1.3/Sum of elements of an array/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+            sum += arr[i];
+        }
+        System.out.println(sum);
     }
 }
