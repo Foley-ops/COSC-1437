6f0135115d5e1dc5718841b482a7554ee7f4c751
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-18 12:17:05 -0500

    working beta

--- CodeZinger Codes/Chapter 11/11.1.2/Read an array of given length/src/Solution.java
+++ CodeZinger Codes/Chapter 11/11.1.2/Read an array of given length/src/Solution.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.print(A[i] + " ");
+        }
     }
 }
