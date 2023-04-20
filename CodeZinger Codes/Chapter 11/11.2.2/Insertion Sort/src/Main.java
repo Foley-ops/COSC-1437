6172fc608235121679953baea36968db4d24e644
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-20 11:13:38 -0500

    added premade code

--- CodeZinger Codes/Chapter 11/11.2.2/Insertion Sort/src/Main.java
+++ CodeZinger Codes/Chapter 11/11.2.2/Insertion Sort/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+// Your program will be evaluated by this Main class and several test cases.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+
+        int A[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = s.nextInt();
+        }
+
+        ProblemSolution problemSolution = new ProblemSolution();
+        System.out.println(problemSolution.solution(A, N));
+    }
+}
+public class ProblemSolution {
+    public int solution(int[] A, int N) {
+
+        //write code here
     }
 }
