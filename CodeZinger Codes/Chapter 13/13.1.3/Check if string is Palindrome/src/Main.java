8036fa0c458745258bc203ddd800c261db02bd09
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-04-26 14:35:35 -0500

    Project completed

---
+++ CodeZinger Codes/Chapter 13/13.1.3/Check if string is Palindrome/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int output = isPalindrome(input) ? 1 : 0;
+        System.out.println(output);
+    }
+
+    public static boolean isPalindrome(String input) {
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
