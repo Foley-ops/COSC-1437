500d7b31415bc8fb1fa5ac3cfd6ad4dc9e3182b1
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-05-01 22:50:34 -0500

    Improve questions in main class

--- Final/DMV Automation/src/DMVKiosk.java
+++ Final/DMV Automation/src/DMVKiosk.java
@@ -5,19 +5,19 @@ public class DMVKiosk {
         Scanner scanner = new Scanner(System.in);
 
         // Obtain user input
-        System.out.println("Enter your full name:");
+        System.out.print("Enter your full name: ");
         String name = scanner.nextLine();
 
-        System.out.println("Enter your date of birth (yyyy-MM-dd):");
+        System.out.print("Enter your date of birth (yyyy-MM-dd): ");
         String dateOfBirth = scanner.nextLine();
 
-        System.out.println("Enter your ID number:");
+        System.out.print("Enter your ID number: ");
         String idNumber = scanner.nextLine();
 
-        System.out.println("Enter your current ID/license issue date (yyyy-MM-dd):");
+        System.out.print("Enter your current ID/license issue date (yyyy-MM-dd): ");
         String oldIssueDate = scanner.nextLine();
 
-        System.out.println("Enter your current ID/license expiration date (yyyy-MM-dd):");
+        System.out.print("Enter your current ID/license expiration date (yyyy-MM-dd): ");
         String oldExpirationDate = scanner.nextLine();
 
         // Close the scanner
